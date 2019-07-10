2c5a6ad023ea2bd4fea6d30242d4cf02fd3d74c6
Orlando Rodrigues <orlandorodriguesti@gmail.com>
2019-07-09 22:37:30 -0300

    Using the webcam

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
  const express = require('express');
+ const Datastore = require('nedb');
  const app = express();
 
  app.listen(3333, () => console.log('Listening at port 3333'));
  app.use(express.static('public'));
  app.use(express.json({limit: '1mb'}));
 
- app.post('/api', (req,res) => {
-    console.log(req.body);
-    res.json({
-        status: 'success'
+ const database  = new Datastore('database.db');
+ database.loadDatabase();
+ 
+ app.get('/api', (request, response) => {
+    database.find({}, (err, data) => {
+      if (err) {
+        response.end();
+        return;
+      }
+      response.json(data);
     });
- });
+  });
+
+  app.post('/api', (request, response) => {
+    const data = request.body;
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    database.insert(data);
+    response.json(data);
+  });
