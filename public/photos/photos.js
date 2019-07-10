a76d55e1d6d2ee10344b4d166ac1f7d2c99e4024
Orlando Rodrigues <orlandorodriguesti@gmail.com>
2019-07-10 20:38:34 -0300

    Final structure

---
+++ public/photos/photos.js
@@ -0,0 +1,21 @@
+async function getData(){
+    const response = await fetch('/api');
+    const data = await response.json();
+
+    for (item of data) {
+        const root = document.createElement('p');
+        const geo = document.createElement('div');
+        const date = document.createElement('div');
+        const image = document.createElement('img');
+
+        geo.textContent = `${item.lat}°, ${item.lon}°`;
+        const dateString = new Date(item.timestamp).toLocaleString();
+        date.textContent = dateString;
+        image.src = item.image64;
+
+        root.append(geo, date, image);
+        document.body.append(root);
+    }
+}
+
+getData();
